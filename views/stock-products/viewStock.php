4e0a2d27d5155b5d78fd88e6da95c4c1de19b4ac
That-VEN <thattven@gmail.com>
2025-03-07 19:25:26 +0700

    fix(add fixing code)POS-77

--- views/stock-products/viewStock.php
+++ views/stock-products/viewStock.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/view.css">
+    <link rel="stylesheet" href="/views/assets/css/view.css">
     <title>Stock Products</title>
     
 </head>
