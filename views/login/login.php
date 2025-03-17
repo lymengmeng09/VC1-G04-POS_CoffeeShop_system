21ee5d29f6e75ecfc41ab6875049adc5af294c45
sreypich01 <sreypich.rom@student.passerellesnumeriques.org>
2025-03-17 20:48:08 +0700

    feat(user): all form error when don't input

--- views/login/login.php
+++ views/login/login.php
@@ -1,5 +1,5 @@
 <section class="h-100 gradient-form" style="background-color: #eee;">
-  <div class="container py-5 h-100">
+  <div class="container h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-xl-10">
         <div class="card rounded-3 text-black">
@@ -12,25 +12,29 @@
                     style="width: 185px;" alt="logo">
                 </div>
 
-                <form method="POST">
+                <form method="POST" id="loginForm" class="needs-validation" novalidate>
                   <p class="text-center">Please login to your account</p>
-
-                  <div data-mdb-input-init class="form-outline mb-4">
+                  <div data-mdb-input-init class="form-outline mb-2">
                     <label class="form-label" for="form2Example11">Email</label>
                     <input type="text" id="form2Example11" class="form-control" name="email"
-                      placeholder="your email" />
+                      placeholder="your email" required />
+                    <div class="invalid-feedback">
+                      Email is required.
+                    </div>
                   </div>
-
-                  <div data-mdb-input-init class="form-outline mb-4">
+                  <div data-mdb-input-init class="form-outline mb-2">
                     <label class="form-label" for="form2Example22">Password</label>
-                    <input type="password" name="password" id="form2Example22" class="form-control" placeholder="password" />
+                    <input type="password" name="password" id="form2Example22" class="form-control" placeholder="password" required />
+                    <div class="invalid-feedback">
+                      Password is required.
+                    </div>
                   </div>
-                  <div class="text-center pt-1 mb-5 pb-1">
-                    <button data-mdb-button-init data-mdb-ripple-init class="btn text-light fa-lg gradient-custom-2 mb-3"  type="submit">Log
+                  <div class="text-center pt-1 mb-4 pb-1">
+                    <button class="btn text-light fa-lg gradient-custom-2 mb-2" type="submit">Log
                       in</button><br>
                     <a class="text-muted" href="#!">Forgot password?</a>
                   </div>
-                  <p class="mt-2 text-center"><a href="/login/register">Don't have an account? Register</a></p>
+                  <p class="text-center"><a href="/login/register">Don't have an account? Register</a></p>
                 </form>
 
               </div>
