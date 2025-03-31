078db625099e2c6be7b4e96d10792ea6429ac222
lymengmeng09 <lymengsv@gmail.com>
2025-03-31 16:00:17 +0700

    style(change style): change style receipt[OPS-86]

--- views/products/list-product.php
+++ views/products/list-product.php
@@ -168,7 +168,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="receiptModalLabel">Order Receipt</h5>
+                        <h5 class="modal-title " id="receiptModalLabel">Order Receipt</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body" id="receipt-content">
